aec0e50ff3259fd2d90938539e523b1435449e41
Ryokuman <ryokuman@naver.com>
2023-02-02 11:43:08 +0900

    feature ( 1.level_1 ) : near_num
    feature ( near_num ) : solution.js
    feature ( near_num ) : otherSolution.js

--- 2.level_1/2.near_num/solution.js
+++ 2.level_1/2.near_num/solution.js
@@ -3,12 +3,17 @@
 
 function solution(s) {
   validator(s);
-  const words = [...s].reduce((acc, cur) => acc.includes(cur), []);
-  return words;
+  const tempArr = Array.from({ length: s.length }, (_, i) => ({ value: s[i], idx: i }));
+  const result = tempArr.map((v) => {
+    const result = [...s.slice(0, v.idx)].reduce((acc, cur, idx) => (cur === v.value ? idx : acc), -1); // 문자열을 0 부터 해당 값의 위치까지 잘라낸다.
+    if (result !== -1) return v.idx - result;
+    return -1;
+  });
+
+  return result;
 }
 
 function validator(s) {
-  if (s.length < 1 || p.length > 18) throw new TypeError(); // 유효성 검사를 통과하지 못할 경우, typeError , 제한사항 1
-  if (t.length < p.length || t.length > 10000) throw new TypeError(); // 유효성 검사를 통과하지 못할 경우, typeError , 제한사항 2
-  if (/[^0-9]/.test(t) || /[^0-9]/.test(p)) throw new TypeError(); // 유효성 검사를 통과하지 못할 경우, typeError , 제한사항 3
+  if (s.length < 1 || s.length > 10000) throw new TypeError(); // 유효성 검사를 통과하지 못할 경우, typeError , 제한사항 1
+  if (/[^a-z]/.test(s)) throw new TypeError(); // 유효성 검사를 통과하지 못할 경우, typeError , 제한사항 2
 }
